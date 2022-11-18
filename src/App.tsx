9b1782839033fd491fb3409f1ab8c9bfa77ce5d8
Gustavo Gomes <gustavosgdev@outlook.com>
2022-11-17 21:02:54 -0400

    first commit! :tada:

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="text-orange-900 text-2xl font-bold">PX TO REM</h1>
+      <p className="text-gray-100 text-sm">Converta facilmente px para rem</p>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
