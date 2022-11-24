4f7142422761b1a8edf2edfc01e3ccfba55a3071
Gustavo Gomes <gustavosgdev@outlook.com>
2022-11-23 20:06:36 -0400

    fix: moves toast to left

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Toaster />
+    <Toaster position="top-right"/>
     <App />
   </React.StrictMode>
 )
